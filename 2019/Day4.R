ef1cb8a4da6e4a01a141ad0d08396ab53df2a99b
unknown <kulumbeg@gmail.com>
2019-12-07 15:15:50 +0100

    Solved Day 4 of AOC2019

--- 2019/Day4.R
+++ 2019/Day4.R
@@ -5,24 +5,74 @@ rm(list = ls())
 input <- readLines("2019/data/Day4.txt")
 input <- as.integer(unlist(strsplit(input,"-")))
 
-password <- function(min = input[1], max = input[2]) {
+passwords <- function(min = input[1], max = input[2]) {
   
   require(stringi)
   require(dplyr)
   vector <- as.character(min:max)
-  dbl_digits <- stringi::stri_count_regex(vector, pattern = "(\\d)\\1+")
-  vector <- vector[which(dbl_digits == 1)]
   
-  vector <- strsplit(vector, "")
-  vector <- lapply(vector, as.integer)
-   
-  return(vector)
+  vh <- tibble(d1 = as.integer(substring(vector, 1, 1)),
+                         d2 = as.integer(substring(vector, 2, 2)),
+                         d3 = as.integer(substring(vector, 3, 3)),
+                         d4 = as.integer(substring(vector, 4, 4)),
+                         d5 = as.integer(substring(vector, 5, 5)),
+                         d6 = as.integer(substring(vector, 6, 6))
+                                         )
+  
+  vh <- vh %>% 
+    filter(d6 >= d5 & d5 >= d4 & d4 >= d3 & d3 >= d2 & d2 >= d1) %>%
+    filter(d1 == d2 | d2 == d3 | d3 == d4 | d4 == d5 | d5 == d6)
+  
+  return(vh)
           
    
 }
 
-password()
+nrow(passwords())
 
+# Explanation
+# The first two conditions (range and 6 digits) are fulfilled by default, so we only check for the other two.
+# The function splits the number in string format and puts every digit in integer form into a separate column.
+# From there, we filter using two criteria: The number in the column to the right can be equal or larger,
+# and numbers in any two adjacent columns must be equal.
 
 
 # Part 2 ------------------------------------------------------------------
+# Process the input in form of XXXXXX-YYYYYY
+rm(list = ls())
+input <- readLines("2019/data/Day4.txt")
+input <- as.integer(unlist(strsplit(input,"-")))
+
+passwords <- function(min = input[1], max = input[2]) {
+  
+  require(stringi)
+  require(dplyr)
+  vector <- as.character(min:max)
+  
+  vh <- tibble(d1 = as.integer(substring(vector, 1, 1)),
+               d2 = as.integer(substring(vector, 2, 2)),
+               d3 = as.integer(substring(vector, 3, 3)),
+               d4 = as.integer(substring(vector, 4, 4)),
+               d5 = as.integer(substring(vector, 5, 5)),
+               d6 = as.integer(substring(vector, 6, 6))
+  )
+  
+  vh <- vh %>% 
+    filter(d6 >= d5 & d5 >= d4 & d4 >= d3 & d3 >= d2 & d2 >= d1) %>%
+    filter((d1 == d2 & d2 != d3)| 
+             (d2 == d3 & (d2 != d1 & d3 != d4)) | 
+             (d3 == d4 & (d3 != d2 & d4 != d5)) | 
+             (d4 == d5 & (d4 != d3 & d5 != d6)) |
+             (d5 == d6 & d5 != d4))
+  
+  return(vh)
+  
+  
+}
+
+nrow(passwords())
+
+# Explanation
+# The same principle with slight modification, in the second filtering condition, we assure that
+# adjacent numbers are the same, but also exclude the cases when adjacent numbers to our compared pair
+# are the same as any one of the numbers in the compared pair.
--- 2019/README.md
+++ 2019/README.md
@@ -1,12 +1,12 @@
 # Advent of Code 2019  
 
-[x] Day 1  | [Solution](2019/Day1.R) | [Data](2019/data/Day1.txt) | [Assignment](2019/assignments/Day1.md)  
-[x] Day 2  | [Solution](2019/Day2.R) | [Data](2019/data/Day2.txt) | [Assignment](2019/assignments/Day2.md)  
-[x] Day 3  | [Solution](2019/Day3.R) | [Data](2019/data/Day3.txt) | [Assignment](2019/assignments/Day3.md) *thanks to [u/turtlegraphics](https://www.reddit.com/user/turtlegraphics/)*    
-[ ] Day 4  | [Solution](2019/Day4.R) | [Data](2019/data/Day4.txt) | [Assignment](2019/assignments/Day4.md)  
-[ ] Day 5  | [Solution](2019/Day5.R) | [Data](2019/data/Day5.txt) | [Assignment](2019/assignments/Day5.md)  
-[x] Day 6  | [Solution](2019/Day6.R) | [Data](2019/data/Day6.txt) | [Assignment](2019/assignments/Day6.md)  
-[ ] Day 7  | [Solution](2019/Day7.R) | [Data](2019/data/Day7.txt) | [Assignment](2019/assignments/Day7.md)  
+[x] Day 1  | [Solution](Day1.R) | [Data](data/Day1.txt) | [Assignment](assignments/Day1.md)  
+[x] Day 2  | [Solution](Day2.R) | [Data](data/Day2.txt) | [Assignment](assignments/Day2.md)  
+[x] Day 3  | [Solution](Day3.R) | [Data](data/Day3.txt) | [Assignment](assignments/Day3.md) *thanks to [u/turtlegraphics](https://www.reddit.com/user/turtlegraphics/)*    
+[ ] Day 4  | [Solution](Day4.R) | [Data](data/Day4.txt) | [Assignment](assignments/Day4.md)  
+[ ] Day 5  | [Solution](Day5.R) | [Data](data/Day5.txt) | [Assignment](assignments/Day5.md)  
+[x] Day 6  | [Solution](Day6.R) | [Data](data/Day6.txt) | [Assignment](assignments/Day6.md)  
+[ ] Day 7  | [Solution](Day7.R) | [Data](data/Day7.txt) | [Assignment](assignments/Day7.md)  
 [ ] Day 8  
 [ ] Day 9  
 [ ] Day 10  
