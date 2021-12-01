752f1223c119a109ff1643a174d4fdbb8b15aad9
Alexander Kulumbeg <kulumbeg@gmail.com>
2021-12-01 07:56:21 +0100

    Day1 of AOC2021 in R - alternative solution

--- 2021/solutionsR/day1.R
+++ 2021/solutionsR/day1.R
@@ -5,9 +5,15 @@ input <- as.integer(readLines("2021/data/day1.txt", warn = F))
 length(which(diff(input) > 0)) 
 # the length of vector of differences which are larger than 0
 
+# an even shorter alternative:
+sum(diff(input) > 0)
+
 # Day 1 - Part 2 ----------------------------------------------------------
 
 input <- as.integer(readLines("2021/data/day1.txt", warn = F))
 
 length(which(diff(input, lag = 3) > 0))
 # the length of vector of differences lagged by 3 which are larger than 0
+
+# an even shorter alternative:
+sum(diff(input, lag = 3) > 0)
